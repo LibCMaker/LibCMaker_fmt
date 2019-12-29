6e8ccafd4e6e615793bf98d5e887b42aa672e31c
NikitaFeodonit <nfeodonit@yandex.com>
2019-12-29 20:46:49 +0300

    Patch Findfmt.cmake

--- cmake/modules/Findfmt.cmake
+++ cmake/modules/Findfmt.cmake
@@ -1,4 +1,9 @@
-find_path(fmt_INCLUDE_DIR NAMES fmt/format.h)
+find_path(fmt_INCLUDE_DIR
+  NAMES fmt/format.h
+  NO_CMAKE_ENVIRONMENT_PATH
+  NO_SYSTEM_ENVIRONMENT_PATH
+  NO_CMAKE_SYSTEM_PATH
+)
 
 if(fmt_INCLUDE_DIR)
   set(_fmt_version_file "${fmt_INCLUDE_DIR}/fmt/core.h")
@@ -8,36 +13,48 @@ if(fmt_INCLUDE_DIR)
   if(EXISTS "${_fmt_version_file}")
     # parse "#define FMT_VERSION 40100" to 4.1.0
     file(STRINGS "${_fmt_version_file}" fmt_VERSION_LINE
-      REGEX "^#define[ \t]+FMT_VERSION[ \t]+[0-9]+$")
+      REGEX "^#define[ \t]+FMT_VERSION[ \t]+[0-9]+$"
+    )
     string(REGEX REPLACE "^#define[ \t]+FMT_VERSION[ \t]+([0-9]+)$"
-      "\\1" fmt_VERSION "${fmt_VERSION_LINE}")
+      "\\1" fmt_VERSION "${fmt_VERSION_LINE}"
+    )
     foreach(ver "fmt_VERSION_PATCH" "fmt_VERSION_MINOR" "fmt_VERSION_MAJOR")
       math(EXPR ${ver} "${fmt_VERSION} % 100")
       math(EXPR fmt_VERSION "(${fmt_VERSION} - ${${ver}}) / 100")
     endforeach()
     set(fmt_VERSION
-      "${fmt_VERSION_MAJOR}.${fmt_VERSION_MINOR}.${fmt_VERSION_PATCH}")
+      "${fmt_VERSION_MAJOR}.${fmt_VERSION_MINOR}.${fmt_VERSION_PATCH}"
+    )
   endif()
 endif()
 
-find_library(fmt_LIBRARY NAMES fmt)
+find_library(fmt_LIBRARY
+  NAMES fmt fmtd
+  NO_CMAKE_ENVIRONMENT_PATH
+  NO_SYSTEM_ENVIRONMENT_PATH
+  NO_CMAKE_SYSTEM_PATH
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(fmt
   REQUIRED_VARS fmt_INCLUDE_DIR fmt_LIBRARY
-  VERSION_VAR fmt_VERSION)
+  VERSION_VAR fmt_VERSION
+)
+
 mark_as_advanced(
   fmt_INCLUDE_DIR
   fmt_LIBRARY
   fmt_VERSION_MAJOR
   fmt_VERSION_MINOR
   fmt_VERSION_PATCH
-  fmt_VERSION_STRING)
+  fmt_VERSION_STRING
+)
 
 if(fmt_FOUND AND NOT (TARGET fmt::fmt))
   add_library(fmt::fmt UNKNOWN IMPORTED)
   set_target_properties(fmt::fmt PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${fmt_INCLUDE_DIR}"
     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
-    IMPORTED_LOCATION "${fmt_LIBRARY}")
+    IMPORTED_LOCATION "${fmt_LIBRARY}"
+  )
 endif()
